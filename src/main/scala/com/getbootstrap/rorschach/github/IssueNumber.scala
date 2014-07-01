86402bd72bc370545614294c08576ce5708d864e
Chris Rebert <code@rebertia.com>
2014-07-01 16:57:50 -0700

    copy over IssueNumber

---
+++ src/main/scala/com/getbootstrap/rorschach/github/IssueNumber.scala
@@ -0,0 +1,15 @@
+package com.getbootstrap.rorschach.github
+
+object IssueNumber {
+  def apply(number: Int): Option[IssueNumber] = {
+    if (number > 0) {
+      Some(new IssueNumber(number))
+    }
+    else {
+      None
+    }
+  }
+}
+class IssueNumber private(val number: Int) extends AnyVal {
+  override def toString = s"IssueNumber(${number})"
+}
