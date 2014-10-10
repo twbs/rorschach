c5f3267b52c624d819fa8768288605ed3be70003
Chris Rebert <code@rebertia.com>
2014-10-10 15:55:56 -0700

    setup_droplet.sh: port: 8080 => 9090

--- setup_droplet.sh
+++ setup_droplet.sh
@@ -20,4 +20,4 @@ ufw status verbose
 # setup Docker; written against Docker v1.2.0
 docker build . 2>&1 | tee docker.build.log
 IMAGE_ID="$(tail -n 1 docker.build.log | cut -d ' ' -f 3)"
-docker run -d -p 80:8080 $IMAGE_ID
+docker run -d -p 80:9090 $IMAGE_ID
